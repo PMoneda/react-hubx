89138f771be83607e28e38722823f0df80f075ab
Philippe Moneda <philippemoneda@gmail.com>
2019-12-23 09:36:20 -0300

    add example

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'hubx'
+import {withHubx} from 'hubx'
 
-export default class App extends Component {
+class App extends Component {
+  
+  constructor(props){
+    super(props)
+    
+    this.onClick = this.onClick.bind(this);
+    this.updateState = this.updateState.bind(this);
+    this.props.attach("my-topic",this.updateState)
+  }
+
+  updateState(number){
+    this.setState(s => {
+      s.number = number;
+      return s;
+    });
+  }
+
+  onClick(){
+    this.props.notify("my-topic",Math.random());
+  }
+  
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        {this.state.number}
+        <button>Generate</button>
       </div>
     )
   }
 }
+
+
+export default  withHubx(App);
